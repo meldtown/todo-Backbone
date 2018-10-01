740b929c0e0935eb7c63576138666f8171c37911
Vitaly <meldtown@gmail.com>
2018-10-01 16:15:48 +0300

    Adding a new TODO

--- views/TodoListView.js
+++ views/TodoListView.js
@@ -3,12 +3,34 @@ app = app || {};
 app.TodoListView = Backbone.View.extend({
   el: '#root',
   template: _.template($('#todo-list').html()),
+  events: {
+    'submit form': 'addTodo',
+    'input .added-todo': 'validateAddedTodo'
+  },
   initialize: function () {
     this.todos = new app.TodoList();
     this.listenTo(this.todos, 'add', this.renderTodo);
     this.listenTo(this.todos, 'reset update', this.render);
     this.todos.fetch({reset: true});
     this.render();
+  },
+  addTodo: function (e) {
+    e.preventDefault();
+    const input = $('.added-todo');
+    const text = input.val().trim();
+    const payload = {text, completed: false};
+    this.todos.create(payload,
+      {
+        wait: true,
+        success: function () {
+          input.val('');
+        },
+        context: this
+      })
+  },
+  validateAddedTodo: function () {
+    const val = $('.added-todo').val().trim();
+    $('.add-todo').attr('disabled', !val);
   },
   render: function () {
     const todos = this.todos.models;
@@ -21,6 +43,8 @@ app.TodoListView = Backbone.View.extend({
     this.todos.each(function (todo) {
       this.renderTodo(todo)
     }, this);
+    const elem = $('.add-todo');
+    elem.attr('disabled', 'true');
   },
   renderTodo: function (model) {
     const todoView = new app.TodoView({model});
